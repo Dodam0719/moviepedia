2abf8922ae4a9592aeec5926c6fd9f937aa5d5a5
Dodam0719 <blueoh719@gmail.com>
2024-02-12 15:30:42 +0900

    feat: 별점 인풋 추가

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./ReviewForm.css";
 import FileInput from "./FileInput";
+import RatingInput from "./RatingInput";
 
 function ReviewForm() {
   const [values, setValues] = useState({
@@ -35,11 +36,10 @@ function ReviewForm() {
         onChange={handleChange}
       />
       <input name="title" value={values.title} onChange={handleInputChange} />
-      <input
-        type="number"
+      <RatingInput
         name="rating"
         value={values.rating}
-        onChange={handleInputChange}
+        onChange={handleChange}
       />
       <textarea
         name="content"
