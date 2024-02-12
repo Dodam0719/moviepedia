3bf0a031025eda7c51fa59b646cbc4ed60fb3db8
Dodam0719 <blueoh719@gmail.com>
2024-02-12 16:08:11 +0900

    feat: 리뷰 생성 API 작동시 로딩처리 및 에러처리 추가

--- src/api.jsx
+++ src/api.jsx
@@ -14,7 +14,7 @@ export async function getReviews({
   return body;
 }
 
-export async function createReviews(formData) {
+export async function createReview(formData) {
   const response = await fetch(`${BASE_URL}/film-reviews`, {
     method: "POST",
     body: formData,
--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { createReviews } from "../api";
+import { createReview } from "../api";
 import "./ReviewForm.css";
 import FileInput from "./FileInput";
 import RatingInput from "./RatingInput";
@@ -13,6 +13,8 @@ const INITIAL_VALUES = {
 
 function ReviewForm() {
   const [values, setValues] = useState(INITIAL_VALUES);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [submittingError, setSubmittingError] = useState(null);
 
   const handleChange = (name, value) => {
     setValues((prevValues) => ({
@@ -33,7 +35,16 @@ function ReviewForm() {
     formData.append("rating", values.rating);
     formData.append("content", values.content);
     formData.append("imgFile", values.imgFile);
-    await createReviews(formData);
+    try {
+      setSubmittingError(null);
+      setIsSubmitting(true);
+      await createReview(formData);
+    } catch (error) {
+      setSubmittingError(error);
+      return;
+    } finally {
+      setIsSubmitting(false);
+    }
     setValues(INITIAL_VALUES);
   };
 
@@ -55,7 +66,10 @@ function ReviewForm() {
         value={values.content}
         onChange={handleInputChange}
       />
-      <button type="submit">확인</button>
+      <button disabled={isSubmitting} type="submit">
+        확인
+      </button>
+      {submittingError && <div>{submittingError.message}</div>}
     </form>
   );
 }
