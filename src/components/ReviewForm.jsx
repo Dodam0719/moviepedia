61e09303c08d846e6a384bf952ad8f56a88c6efc
Dodam0719 <blueoh719@gmail.com>
2024-02-12 15:59:19 +0900

    feat: 리뷰 생성 API를 연동

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
+import { createReviews } from "../api";
 import "./ReviewForm.css";
 import FileInput from "./FileInput";
 import RatingInput from "./RatingInput";
 
+const INITIAL_VALUES = {
+  title: "",
+  rating: 0,
+  content: "",
+  imgFile: null,
+};
+
 function ReviewForm() {
-  const [values, setValues] = useState({
-    title: "",
-    rating: 0,
-    content: "",
-    imgFile: null,
-  });
+  const [values, setValues] = useState(INITIAL_VALUES);
 
   const handleChange = (name, value) => {
     setValues((prevValues) => ({
@@ -23,9 +26,15 @@ function ReviewForm() {
     handleChange(name, value);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(values);
+    const formData = new FormData();
+    formData.append("title", values.title);
+    formData.append("rating", values.rating);
+    formData.append("content", values.content);
+    formData.append("imgFile", values.imgFile);
+    await createReviews(formData);
+    setValues(INITIAL_VALUES);
   };
 
   return (
