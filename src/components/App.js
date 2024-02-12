6ce01fe7f9c25f91f20581c30d9f8beab73de489
Dodam0719 <blueoh719@gmail.com>
2024-02-12 14:15:53 +0900

    feat: 입력폼 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import ReviewList from "./ReviewList";
+import ReviewForm from "./ReviewForm";
 import { getReviews } from "../api";
 import { useEffect, useState } from "react";
 
@@ -59,6 +60,7 @@ function App() {
         <button onClick={handleNewestClick}>최신순</button>
         <button onClick={handleBestClick}>베스트순</button>
       </div>
+      <ReviewForm />
       <ReviewList items={sortedItems} onDelete={handleDelete} />
       {hasNext && (
         <button disabled={isLoading} onClick={handleLoadMore}>
